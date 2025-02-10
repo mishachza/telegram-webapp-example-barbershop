0b687defe1e317a21c75b083132240775fe03050
mishachza <mishachza@ya.ru>
2025-02-10 11:45:44 +0300

    master details

--- barbershopwebapp/vue.config.js
+++ barbershopwebapp/vue.config.js
@@ -1,11 +1,11 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/telegram-webapp-example-barbershop/'
-    : '/'
-})
+ const { defineConfig } = require('@vue/cli-service')
+ module.exports = defineConfig({
+   transpileDependencies: true
+ })
+//const { defineConfig } = require('@vue/cli-service')
+//module.exports = defineConfig({
+//  transpileDependencies: true,
+//  publicPath: process.env.NODE_ENV === 'production'
+//    ? '/telegram-webapp-example-barbershop/'
+//    : '/'
+//})
